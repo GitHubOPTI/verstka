2b3d0746058a5fee55a3cdac2bf548abf975a7ab
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-06-28 10:50:24 +0500

    menu fix

--- lllink-marketing/js/main.js
+++ lllink-marketing/js/main.js
@@ -183,9 +183,17 @@ if (items.length > maxiItemsCount - 1) {
 
 const menu = document.querySelector(".menu");
 const burger = document.querySelector(".burger");
+const body = document.querySelector(".page__body");
 burger.addEventListener("click", () => {
   menu.classList.toggle("menu-active");
   burger.classList.toggle("burger--active");
+  if (body.style.overflowY == "hidden") {
+    body.style.overflowY = "unset";
+    body.style.paddingRight = "0";
+  } else {
+    body.style.paddingRight = window.innerWidth - body.offsetWidth + "px";
+    body.style.overflowY = "hidden";
+  }
 });
 
 /***/ }),
