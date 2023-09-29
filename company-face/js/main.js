93994133bcf1a423e65bc45eefd53f75061a4ff7
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-09-29 15:23:02 +0500

    company-face project

---
+++ company-face/js/main.js
@@ -0,0 +1,14 @@
+/******/ (() => {
+	// webpackBootstrap
+	/******/ "use strict";
+	var __webpack_exports__ = {}; // CONCATENATED MODULE: ./src/js/_vars.js
+
+	/* harmony default export */ const _vars = {
+		windowEl: window,
+		documentEl: document,
+		htmlEl: document.documentElement,
+		bodyEl: document.body,
+	}; // CONCATENATED MODULE: ./src/js/main.js
+
+	/******/
+})();
